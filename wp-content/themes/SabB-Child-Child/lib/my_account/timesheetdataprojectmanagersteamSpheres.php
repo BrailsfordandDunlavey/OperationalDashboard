360a817ae43af31cb184a5cfdd417c5035fc7e37
hcadet <58520366+hcadet@users.noreply.github.com>
2020-06-25 13:47:52 -0400

    Update timesheetdataprojectmanagersteamSpheres.php

--- wp-content/themes/SabB-Child-Child/lib/my_account/timesheetdataprojectmanagersteamSpheres.php
+++ wp-content/themes/SabB-Child-Child/lib/my_account/timesheetdataprojectmanagersteamSpheres.php
@@ -78,7 +78,7 @@ $spheres_arr = array("Functional", "Higher Ed", "KMV");
 
 foreach($spheres_arr as $sphk => $sphv){
 $team_management_position = array();
-/	$table_emp_sup = 'useradd';
+	$table_emp_sup = 'useradd';
 		$client_table_emp = 'users';
 		$table_name_emp_sup = $wpdb->prefix . $table_emp_sup;
 		$client_table_name_emp = $wpdb->prefix . $client_table_emp; 
