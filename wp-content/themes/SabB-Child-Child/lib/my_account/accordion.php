af07804ca043793a70ab400da03c7d16b89342fb
hcadet <58520366+hcadet@users.noreply.github.com>
2020-07-22 10:57:46 -0400

    Update accordion.php

--- wp-content/themes/SabB-Child-Child/lib/my_account/accordion.php
+++ wp-content/themes/SabB-Child-Child/lib/my_account/accordion.php
@@ -1,30 +1,30 @@
 <style>
 
- /* Style the buttons that are used to open and close the accordion panel */
-.accordion {
-  background-color: #eee;
-  color: #444;
-  cursor: pointer;
-  padding: 18px;
-  width: 200%;
-  text-align: left;
-  border: none;
-  outline: none;
-  transition: 0.4s;
+details {
+    border: 1px solid #aaa;
+    border-radius: 4px;
+    padding: .75em .75em 0;
+    width: 200%;
 }
 
-/* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
-.active, .accordion:hover {
-  background-color: #ccc;
+summary {
+    font-weight: bold;
+    margin: -.75em -.75em 0;
+    padding: .75em;
+    background-color: steelblue;
+    color: white;
 }
 
-/* Style the accordion panel. Note: hidden by default */
-.panel {
-  padding: 0 18px;
-  background-color: white;
-  display: none;
-  overflow: hidden;
+details[open] {
+    padding: .75em;
 }
+
+details[open] summary {
+    border-bottom: 1px solid #aaa;
+    margin-bottom: .75em;
+}
+
+
 </style>
 
 <?php 
@@ -48,10 +48,10 @@
 	   $result_staff = $wpdb->get_results(
 		"SELECT * FROM $table_name_staff		    
             WHERE Team LIKE '%$team%'");  
-    
-       echo"<button class='accordion'>$team</button>
-            <div class='panel'>
-            ";
+    echo "<details><summary>$team</summary>";
+      // echo"<button class='accordion'>$team</button>
+            //<div class='panel'>
+           // ";
             foreach($result_staff as $key_staff => $val_staff){
                 $staff_name = $val_staff->Name;
                 //print_r(explode($staff_name));
@@ -66,11 +66,10 @@
                  $nameID = $result_name[0]->ID;
                  echo "<p><a href=http://localhost/wp-opdash/dashboard/?current_user=$nameID>$staff_name</a></p>";
             }             
-            echo "</div>";
+            //echo "</div>";
+            echo "</details>";
     }
 ?>
-
-
 <script>
 var acc = document.getElementsByClassName("accordion");
 var i;
