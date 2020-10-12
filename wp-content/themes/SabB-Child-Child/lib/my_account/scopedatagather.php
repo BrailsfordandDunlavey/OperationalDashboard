75c35291cb9556351552dccc127cd42ecf1268b3
hcadet <58520366+hcadet@users.noreply.github.com>
2020-10-11 23:24:21 -0400

    Needed files for the redirect

--- wp-content/themes/SabB-Child-Child/lib/my_account/scopedatagather.php
+++ wp-content/themes/SabB-Child-Child/lib/my_account/scopedatagather.php
@@ -1,5 +1,6 @@
  <?php
-
+    session_start();
+    
 //include( 'PLUGIN_ROOT_DIR' . 'formidable/classes/models/FrmEntry.php');
 //include('../../plugins/formidable/classes/models/FrmEntry.php');
 if ( !defined('ABSPATH') ) {
@@ -12,12 +13,12 @@ include( $plugin_dir_path . 'plugins/formidable/classes/models/FrmEntry.php');
 //echo $plugin_dir_path;
     //echo "Hello World";
 
-    print_r($_GET);
+    //print_r($_GET);
 
     $entries = FrmEntry::getAll(array('it.form_id' => 6), ' ORDER BY it.created_at DESC', 8);
-    echo "<pre>";
+    //echo "<pre>";
     //
-    echo "</pre>";
+    //echo "</pre>";
     add_action('frm_trigger_my_action_name_create_action', 'my_create_action_trigger', 10, 3);
     function my_create_action_trigger($action, $entry, $form) {
         print_r($entry);
@@ -29,6 +30,85 @@ include( $plugin_dir_path . 'plugins/formidable/classes/models/FrmEntry.php');
     FrmProEntriesController::get_field_value_shortcode(array('field_id' => 6, 'entry' => $entry_id));
 
     $entries = FrmProEntriesController::get_field_value_shortcode(array('field_id' => 6, 'user_id' => 'current'));
-    print_r($entries);
+    //print_r($entries);
     $entries = FrmProEntriesController::get_field_value_shortcode(array('field_id' => 9, 'user_id' => 'current'));
+
+    global $wpdb;
+    $table = 'frm_item_metas';	
+	$table_name = $wpdb->prefix . $table;	
+	$result = $wpdb->get_results(
+		"SELECT * FROM $table_name");
+            
+    //echo "<pre>";
+    //print_r($result);
+    foreach($result as $ekey => $eval){
+       // echo "<br />";
+         //print_r($eval->meta_value);
+         switch ($eval->field_id) {
+          case 28:
+                $pa_arr[] = $eval->meta_value * 200;
+                $pa_arr_two[] = $eval->meta_value;
+                break;
+            case 31:
+                $apm_arr[] = $eval->meta_value * 210;
+                $apm_arr_two[] = $eval->meta_value;
+                break;
+            case 33:
+                $pm_arr[] = $eval->meta_value * 240;
+                $pm_arr_two[] = $eval->meta_value;
+                break;
+            case 35:
+                $vp_arr[] = $eval->meta_value * 360;
+                $vp_arr_two[] = $eval->meta_value;
+                break;                
+            case 36:
+                $evp_arr[] = $eval->meta_value * 455;
+                $evp_arr_two[] = $eval->meta_value;
+                break;            
+
+             /* 
+                case 14:
+                    $pa_arr[] = $eval->meta_value * 200;
+                    $pa_arr_two[] = $eval->meta_value;
+                    break;
+                case 15:
+                    $apm_arr[] = $eval->meta_value * 210;
+                    $apm_arr_two[] = $eval->meta_value;
+                    break;
+                case 21:
+                    $pm_arr[] = $eval->meta_value * 240;
+                    $pm_arr_two[] = $eval->meta_value;
+                    break;
+                case 22:
+                    $vp_arr[] = $eval->meta_value * 360;
+                    $vp_arr_two[] = $eval->meta_value;
+                    break;                
+                case 24:
+                    $evp_arr[] = $eval->meta_value * 455;
+                    $evp_arr_two[] = $eval->meta_value;
+                    break;*/
+                
+        }
+    }
+  //  echo "</pre>";
+
+    //print_r($pa_arr_two);
+   // 
+    
+    for($i = 0; $i < sizeof($pa_arr); $i++){
+        $summed_entries[] = $pa_arr[$i] + $apm_arr[$i] + $vp_arr[$i] + $evp_arr[$i] + $epa_arr[$i] + $pm_arr[$i];       
+    }
+    
+    //$total_fees = array_sum($summed_entries);
+   // print_r($summed_entries);
+
+    $_SESSION['summed_entries'] = $summed_entries;    
+    $_SESSION['pa_arr_two'] = $pa_arr_two;
+    $_SESSION['apm_arr_two'] = $apm_arr_two;
+    $_SESSION['pm_arr_two'] = $pm_arr_two;
+    $_SESSION['vp_arr_two'] = $vp_arr_two;
+    $_SESSION['evp_arr_two'] = $evp_arr_two;
+    
+    header("Location: http://localhost/wpopdash/scopes-information/");
+    
 ?>
---
+++ wp-content/themes/SabB-Child-Child/scopesinfotemplate.php
@@ -0,0 +1,103 @@
+<?php /* Template Name: Scopes Information */ 
+session_start(); 
+get_header();
+	global $post;
+ 
+?>
+<div class="page_heading_me">
+	<div class="page_heading_me_inner">
+    <div class="main-pg-title">
+    	<div class="mm_inn"><?php
+						
+							echo $post->post_title
+						
+						?>
+                     </div>
+                    
+        
+<?php 
+    
+     
+     
+
+		if(function_exists('bcn_display'))
+		{
+		    echo '<div class="my_box3_breadcrumb breadcrumb-wrap">';	
+		    bcn_display();
+			echo '</div>';
+		}
+
+?>	</div>
+
+
+		<?php projectTheme_get_the_search_box() ?>            
+                    
+    </div>
+</div>
+
+
+<?php projecttheme_search_box_thing() ?>
+
+<!-- ########## -->
+
+<div id="main_wrapper">
+		<div id="main" class="wrapper"> 
+
+
+
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<?php the_content(); ?>			
+<?php endwhile; // end of the loop. ?> 
+
+<?php print_r($summed_entries);   
+  $total_fees = array_sum($_SESSION['pa_arr']);  
+  echo  '<style rel="stylesheet" type="text/css">
+
+  th {
+    height: 50px;
+    font-weight: bold;
+    text-align: left;
+    background-color: #cccccc;
+  }
+  
+  
+  
+  </style>';
+
+   //print_r($_SESSION['summed_entries']); 
+  //echo $_SESSION['pa_arr'][$j];
+   $in = 1;
+   echo "<table><tr>";
+        echo "<th>Task Category</th><th>PA</th><th>APM</th><th>PM</th><th>VP</th><th>EVP</th><th>Total Fees</th></tr>";
+   for($i=0; $i<5; $i++){
+    echo "<tr><td>";
+        echo"Scope Item " .$in;
+    echo "</td>";
+    echo "<td>";
+        echo $_SESSION['pa_arr_two'][$i];
+    echo "</td>";
+    echo "<td>";
+        echo $_SESSION['apm_arr_two'][$i];
+    echo "</td>";
+    echo "<td>";
+        echo $_SESSION['pm_arr_two'][$i];
+    echo "</td>";
+    echo "<td>";
+        echo $_SESSION['vp_arr_two'][$i];
+    echo "</td>";
+    echo "<td>";
+        echo $_SESSION['evp_arr_two'][$i];
+    echo "</td>"; 
+    echo "<td>";
+        echo $_SESSION['summed_entries'][$i];
+    echo "</td>";  
+    echo "</tr>"; 
+    $in++;
+   }
+   echo "</table>"
+?>
+ 
+</div>
+</div>
+<?php get_footer(); ?>
